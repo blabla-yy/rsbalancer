8a23892ac7b0cdc0a0b60838a9fa256e8666e43a
blabla-yy <yahaha7777@gmail.com>
2023-08-15 20:52:07 +0800

    remove unused

--- src/errors.rs
+++ src/errors.rs
@@ -2,36 +2,10 @@ use std::error::Error;
 use std::fmt;
 
 #[derive(Debug)]
-pub struct ParameterError {
-    message: String,
-}
-
-#[derive(Debug)]
 pub struct NotFoundError;
 
 #[derive(Debug)]
 pub struct DuplicatedKeyError;
-
-
-impl ParameterError {
-    pub fn new(message: &str) -> ParameterError {
-        ParameterError {
-            message: message.to_string(),
-        }
-    }
-}
-
-impl fmt::Display for ParameterError {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "ParameterError: {}", self.message)
-    }
-}
-
-impl Error for ParameterError {
-    fn description(&self) -> &str {
-        &self.message
-    }
-}
 
 
 impl fmt::Display for NotFoundError {
